cc00cd6af3a78952522fcedb5dc8c500c9ca6c42
zchengfa <1522977814@qq.com>
2023-08-31 21:25:07 +0800

    修改消息列表组件内容、添加自定义icon组件

--- src/common/staticData/data.tsx
+++ src/common/staticData/data.tsx
@@ -75,3 +75,26 @@ export const correctIconComponent = [
     component:()=> <UserAddOutlined />
   }
 ]
+
+export const messageData = [
+  {
+    id:3156987,
+    user:'阿藏',
+    type:'text',
+    msg:'这是一条用于测试的聊天消息',
+    avatar:'',
+    time:'刚刚',
+    isMute:false,
+    hasBeenRead:false
+  },
+  {
+    id:5678453,
+    user:'沙雕',
+    type:'text',
+    msg:'你好',
+    avatar:'',
+    time:'08/10',
+    isMute:true,
+    hasBeenRead:false
+  }
+]
--- src/components/ChatList/ChatList.tsx
+++ src/components/ChatList/ChatList.tsx
@@ -1,50 +1,27 @@
 import './chatList.sass'
 import { Avatar,Badge,Space } from "antd";
 import { UserOutlined } from '@ant-design/icons'
-
+import { BellIconComponent } from '../../common/svg/svg'
+import { messageData } from '../../common/staticData/data'
 
 export default function ChatList (){
-  const messageData = [
-    {
-      id:3156987,
-      user:'阿藏',
-      type:'text',
-      msg:'这是一条用于测试的聊天消息',
-      avatar:'',
-      time:'刚刚',
-      isMute:false,
-      hasBeenRead:false
-    },
-    {
-      id:5678453,
-      user:'沙雕',
-      type:'text',
-      msg:'你好',
-      avatar:'',
-      time:'08/10',
-      isMute:false,
-      hasBeenRead:false
-    }
-  ]
   const chatListElement = ()=>{
     return messageData.map((item:any,index:number)=>{
       return <li key={index}>
         <div className={'message-box'}>
-          <Space>
+          <Space className={'msg-left'}>
             <Badge dot={!item.hasBeenRead}>
               <Avatar shape={'square'} icon={<UserOutlined />}></Avatar>
             </Badge>
           </Space>
-          <Space>
-            <div className={'chat-item-right'}>
-              <div>
-                <p>{item.user}</p>
-                <p>{item.msg}</p>
-              </div>
-              <div>
-                <p>{item.time}</p>
-                <p>{item.isMute ? '静音':'未静音'}</p>
-              </div>
+          <Space className={'msg-right'}>
+            <div className={'user-msg'}>
+              <p className={'chat-user'}>{item.user}</p>
+              <p className={'chat-msg'}>{item.msg}</p>
+            </div>
+            <div className={'time-mute'}>
+              <p className={'chat-time'}>{item.time}</p>
+              {item.isMute ? <BellIconComponent className={'chat-mute'} /> : null}
             </div>
           </Space>
         </div>
@@ -52,9 +29,7 @@ export default function ChatList (){
     })
   }
 
-  return <div>
-    <ul>
-      {chatListElement()}
-    </ul>
-  </div>
+  return <ul className={'list-container'}>
+    {chatListElement()}
+  </ul>
 }

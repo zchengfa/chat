078c1c0ec864507295bd587ac46478a385d2adfe
zchengfa <1522977814@qq.com>
2023-09-12 13:04:30 +0800

    完善WaterDroplet组件

--- src/components/WaterDroplet/WaterDroplet.tsx
+++ src/components/WaterDroplet/WaterDroplet.tsx
@@ -1,27 +1,45 @@
 import './waterDroplet.sass'
-import { Form,Input,Button } from "antd";
+import { Form,Input,Button,notification} from "antd";
 
 function WaterDroplet(props:any){
+    const [api,contextHolder,] = notification.useNotification()
+    const key = 'updatable'
+    const onFinish = (values:any)=>{
+        console.log(values)
+    }
+
+    const onFinishFailed = ()=>{
+        api.open({
+            key,
+            message: 'Form Error',
+            description: 'Please fill out the form content according to regulations.',
+        })
+
+    }
 
     return <div className={'drop-container'}>
+        {contextHolder}
         {
+
             props.type === 'form' ? <div className={'drop'} style={{minHeight:'400px',boxShadow:props.dropBoxShadow,width:props.width,height:props.height,backgroundColor:props.waterDropletColor}}>
                 <p className={'form-title'} style={{fontSize:props.fontSize}}>{props.title ? props.title : 'title'}</p>
-                <Form className={'form'}>
-                    <Form.Item className="form-item">
-                        <span className={'form-input-span'}></span>
+                <Form className={'form'}
+                      onFinish={onFinish}
+                      onFinishFailed={onFinishFailed}
+                >
+                    <Form.Item className="form-item" name={'username'} validateTrigger="onBlur" rules={[{required:true,message:'please enter your username'}]}>
                         <Input className={'account form-input'} placeholder={'Username'}></Input>
                     </Form.Item>
-                    <Form.Item className="form-item password">
-                        <span className={'form-input-span'}></span>
+                    <Form.Item className="form-item password" name={'password'} validateTrigger="onBlur" rules={[{required:true,message:'please enter your password'}]}>
                         <Input className={'password form-input'} placeholder={'Password'}></Input>
                     </Form.Item>
                     <Form.Item className={'form-item'}>
-                        <Button className={'form-input form-btn'}>LOGIN</Button>
+                        <Button htmlType={'submit'} className={'form-input form-btn'}>LOGIN</Button>
                     </Form.Item>
                 </Form>
             </div>: <div className={'drop'} style={{left:'0',top:'0',boxShadow:props.dropBoxShadow,width:props.width,height:props.height,borderRadius:props.radius,backgroundColor:props.waterDropletColor}}>
-                <span className={'form-title'} style={{position:'relative',top:'0%',maxWidth:'50%',textAlign:'center',fontSize:props.fontSize}}>{props.title ? props.title : 'title'}</span>
+                <span className={'drop-before'} style={{backgroundColor:props.preCircleBgColor}}></span>
+                <span className={'form-title'} style={{position:'relative',top:'0%',maxWidth:'50%',textAlign:'center',fontSize:props.fontSize,color:props.color}}>{props.title ? props.title : 'title'}</span>
             </div>
         }
     </div>
--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -9,12 +9,16 @@ function Login(){
                           title={'Sign in'}
                           width={'400px'}
                           height={'400px'}
+                          waterDropletColor={'rgba(231 221 221,.5)'}
             ></WaterDroplet>
         </div>
         <div className={'drop-two'} style={{width:'200px',height:'200px'}}>
             <WaterDroplet width={'200px'} height={'200px'} title={'FORGET PASSWORD'}
                           radius={'70% 30% 38% 62% / 21% 58% 42% 79%'} fontSize={'12px'}
-                          waterDropletColor={'red'}
+                          waterDropletColor={'#ff00bf'}
+                          dropBoxShadow={'inset 20px 20px 20px rgba(0,0,0,.05),20px 20px 20px rgba(0,0,0,.05),20px 20px 20px rgba(255,0,191,0.5),inset -20px -20px 25px rgba(255,255,255,.9)'}
+                          color={'#fff'}
+                          preCircleBgColor={'rgba(255,0,191,0.5)'}
             >
 
             </WaterDroplet>
@@ -22,7 +26,10 @@ function Login(){
         <div className={'drop-three'} style={{width:'140px',height:'140px'}}>
             <WaterDroplet width={'140px'} height={'140px'} title={'SIGN UP'}
                           radius={'45% 55% 70% 30% / 21% 58% 42% 79%'} fontSize={'12px'}
-                          waterDropletColor={'blue'}
+                          waterDropletColor={'#00b0ff'}
+                          dropBoxShadow={'inset 20px 20px 20px rgba(0,0,0,.05),20px 20px 20px rgba(0,0,0,.05),20px 20px 20px rgba(0,176,255,0.5),inset -20px -20px 25px rgba(255,255,255,.9)'}
+                          color={'#fff'}
+                          preCircleBgColor={'rgba(0,176,255,0.5)'}
             >
 
             </WaterDroplet>

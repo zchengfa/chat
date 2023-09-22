e7e9368581ab79a07952c5858bd86c5b05e4f822
zchengfa <1522977814@qq.com>
2023-09-22 14:47:07 +0800

    修改请求端口

--- src/network/network.tsx
+++ src/network/network.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { Navigate } from "react-router-dom";
 
-const baseURL = 'http://localhost:3000'
+const baseURL = 'http://localhost:4000'
 const timeout = 5000
 export function Get(config:any){
     const instance = axios.create({
--- src/socket/socket.ts
+++ src/socket/socket.ts
@@ -1,4 +1,4 @@
 import { io } from 'socket.io-client'
 
 
-export const socket = io('ws://localhost:3000')
+export const socket = io('ws://localhost:4000')

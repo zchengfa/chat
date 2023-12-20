d744b74056c62bf790dab59577dcca63571e4154
zchengfa <1522977814@qq.com>
2023-12-20 19:44:41 +0800

    debug

--- src/pages/home/CollectionList/CollectionList.tsx
+++ src/pages/home/CollectionList/CollectionList.tsx
@@ -2,13 +2,13 @@ import './collectionList.sass'
 import {collectionMenu} from "../../../common/staticData/data";
 import {List} from "antd";
 import {useState} from "react";
-import {UpOutlined,DownOutlined} from '@ant-design/icons'
+import {UpOutlined, DownOutlined} from '@ant-design/icons'
 
-export function CollectionList (props:any){
-  const [isOpen,setIsOpen] = useState(false)
+export function CollectionList(props: any) {
+  const [isOpen, setIsOpen] = useState(false)
 
-  const btnClick = (index:number)=>{
-    if(index === 3){
+  const btnClick = (index: number) => {
+    if (index === 3) {
       setIsOpen(!isOpen)
     }
   }
@@ -17,20 +17,21 @@ export function CollectionList (props:any){
                itemLayout={'vertical'}
                dataSource={collectionMenu}
                split={false}
-               renderItem={(item:any,index:number)=>{
-                 return <List.Item onClick={()=> btnClick(index)} className={'menu-item'} style={item.type ? {justifyContent:'center'} : {}}>
+               renderItem={(item: any, index: number) => {
+                 return <List.Item onClick={() => btnClick(index)} className={'menu-item'}
+                                   style={item.type ? {justifyContent: 'center'} : {}}>
                    {
 
                      index !== 3 ? <div className={'item-box'}>
                        {item.icon}
                        <span className={'item-title'}>{item.title}</span>
                      </div> : <div className={'item-box'}>
-                       <div>
+                       <div style={{display:'flex',alignItems:'center'}}>
                          {item.icon}
                          <span className={'item-title'}>{item.title}</span>
                        </div>
                        {
-                         isOpen ? <DownOutlined className={'up-down'} /> : <UpOutlined className={'up-down'}/>
+                         isOpen ? <DownOutlined className={'up-down'}/> : <UpOutlined className={'up-down'}/>
                        }
                      </div>
                    }

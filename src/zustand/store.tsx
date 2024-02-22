bc0e590b6f5cbe50b2442f3f224fa555facf4025
随风 <1522977814@qq.com>
2024-02-22 06:49:08 +0000

    Revert "debug"
    This reverts commit 4e13e99e1a76c48e14f8e8c2ccc1cfe8f78a5785.

--- src/zustand/store.tsx
+++ src/zustand/store.tsx
@@ -48,29 +48,19 @@ function operateIndexedDB(id:any,data:any){
     let db = DB.db
     id = Number(id) ? Number(id) : id
     getDataByCursorIndex(db, 'chat', true, 'userId', id).then((res: any) => {
-      // if (res.list.length) {
-      //   deleteDataByCursorIndex(db, 'chat', 'userId', id).then(() => {
-      //     updateDB(db, 'chat', {
-      //       userId: id,
-      //       messages: data[id]
-      //     }).then(() => {
-      //       closeDB(db)
-      //     })
-      //   }).catch(() => {
-      //     closeDB(db)
-      //   })
-      //
-      // } else {
-      //   updateDB(db, 'chat', {
-      //     userId: id,
-      //     messages: data[id]
-      //   }).then(() => {
-      //     closeDB(db)
-      //   }).catch((e:any)=>{
-      //     console.log(e)
-      //   })
-      // }
-      deleteDataByCursorIndex(db, 'chat', 'userId', id).then(() => {
+      if (res.list.length) {
+        deleteDataByCursorIndex(db, 'chat', 'userId', id).then(() => {
+          updateDB(db, 'chat', {
+            userId: id,
+            messages: data[id]
+          }).then(() => {
+            closeDB(db)
+          })
+        }).catch(() => {
+          closeDB(db)
+        })
+
+      } else {
         updateDB(db, 'chat', {
           userId: id,
           messages: data[id]
@@ -79,9 +69,7 @@ function operateIndexedDB(id:any,data:any){
         }).catch((e:any)=>{
 
         })
-      }).catch(() => {
-        closeDB(db)
-      })
+      }
     })
 
   })
@@ -389,7 +377,7 @@ export const useMessageStore = create((set) => {
         let data = allList[state.customer.user_id] ? allList[state.customer.user_id] : []
 
         if (replyId && !isReceive) {
-          let index: any = 0
+          let index: any = undefined
           data.map((it: any, i: number) => {
             let myId = item.isGroupChat ? it.room : it.userId
 

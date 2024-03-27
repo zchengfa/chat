f681acef8f74c05e897a69b49a109895a24b69ea
zchengfa <1522977814@qq.com>
2024-03-27 10:40:39 +0800

    feat(添加顶部导航组件):

---
+++ src/components/Common/NavBar/NavBar.tsx
@@ -0,0 +1,36 @@
+import './NavBar.sass'
+import withHook from "../../../hook/withHook";
+import {Navbar, Container, Nav,NavDropdown} from "react-bootstrap";
+import {NavBarData} from "../../../common/staticData/data";
+
+function NavBar(props:any){
+  const addClick = ()=>{
+
+  }
+  return <Navbar expand>
+    <Container fluid>
+      <Navbar.Collapse id={'responsive-navbar-nav'}>
+        <Nav className={'nav'}>
+          {props.back ? <div className={'nav-back-box nav-item'}>{NavBarData.back.icon}</div> : <div className={'nav-item'}></div>}
+          <div className={'nav-center'}>{props.title}</div>
+          {props.more ? <div className={'nav-more-box nav-item'}>{NavBarData.more.icon}</div> : null}
+          {props.add ? <div className={'nav-add-box nav-item'} onClick={addClick}>
+            <NavDropdown title={NavBarData.add.icon} className={'dropdown-nav'}>
+              {
+                NavBarData.dropDownList.map((item:any,index:number)=>{
+                  return <NavDropdown.Item key={index}>
+                    {item.icon}
+                    <span className={'dropdown-item-title'}>{item.title}</span>
+                  </NavDropdown.Item>
+                })
+              }
+            </NavDropdown>
+          </div> : null}
+        </Nav>
+      </Navbar.Collapse>
+    </Container>
+  </Navbar>
+}
+
+export default withHook(NavBar)
+
